295883fdfae3ae153983decc14c3db2a19f6518b
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2013-03-03 19:09:16 -0500

    start to implement splitpatch

--- lib/MJD/GitUtil.pm
+++ lib/MJD/GitUtil.pm
@@ -1,9 +1,32 @@
 package MJD::GitUtil;
 use base 'Exporter';
 our @EXPORT_OK = qw(split_patch);
+use MJD::GitUtil::Patch;
 
 sub split_patch {
-    return ();
+  my ($file) = @_;
+  my $patch = MJD::GitUtil::Patch->new({ file => $file });
+  for my $file ($patch->files) {
+    my $orig_path = $file->path;
+    (my $chunk_path = $orig_path) =~ s{/}{::}g;
+    my $seqno = 1;
+    for my $chunk ($file->chunks) {
+      write_chunk("$chunk_path-$seqno", $orig_path, $chunk);
+      $seqno++;
+    }
+  }
+}
+
+sub write_chunk {
+  my ($file, $path, $chunk) = @_;
+  open my($f), ">", $file
+    or die "Couldn't open '$file' for writing: $!";
+
+  # or maybe use the dpaths here?
+  print $f "--- a/$path\n";
+  print $f "+++ b/$path\n";
+
+  print $f map("$_\n", @{$chunk});
 }
 
 1;
--- lib/MJD/GitUtil/Patch/File.pm
+++ lib/MJD/GitUtil/Patch/File.pm
@@ -1,6 +1,12 @@
 package MJD::GitUtil::Patch::File;
 use Moo;
 
+has path => ( is => 'ro' );
+has shas => ( is => 'ro' );
+has dpaths => ( is => 'ro' );
+has mode => ( is => 'ro' );
+has chunks => ( is => 'ro' );
+
 sub num_chunks {
   my ($self) = @_;
   return 0 + @{$self->{chunks}};
