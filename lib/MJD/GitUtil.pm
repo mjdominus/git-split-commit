cdd5d12290864a7f3f43b2467890dc4c8b95c95c
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2013-03-05 13:24:10 -0500

    return value from split_patch

--- lib/MJD/GitUtil.pm
+++ lib/MJD/GitUtil.pm
@@ -4,17 +4,22 @@ our @EXPORT_OK = qw(split_patch);
 use MJD::GitUtil::Patch;
 
 sub split_patch {
-  my ($file) = @_;
+  my ($file, $dir) = @_;
   my $patch = MJD::GitUtil::Patch->new({ file => $file });
+  my %seqno;
+  my @result_files;
   for my $file ($patch->files) {
     my $orig_path = $file->path;
     (my $chunk_path = $orig_path) =~ s{/}{::}g;
-    my $seqno = 1;
     for my $chunk ($file->chunks) {
-      write_chunk("$chunk_path-$seqno", $orig_path, $chunk);
-      $seqno++;
+      my $seqno = ++$seqno{$chunk_path};
+      my $file = "$chunk_path-$seqno";
+      $file = "$dir/$file" if defined $dir;
+      write_chunk($file, $orig_path, $chunk);
+      push @result_files, $file;
     }
   }
+  return @result_files;
 }
 
 sub write_chunk {
